d763e5730c73d45cfff303b94b3df1a0a1f24a6a
qichangjian <17863860441@163.com>
2018-10-18 08:49:01 +0800

    second commit

--- src/main/java/com/qcj/map_reduce/Map.java
+++
@@ -1,5 +0,0 @@
-package com.qcj.map_reduce;
-
-public interface Map {
-    java.util.Map<String,Integer> map(String path);
-}
--- src/main/java/com/qcj/map_reduce/Reduce.java
+++
@@ -1,7 +0,0 @@
-package com.qcj.map_reduce;
-
-import java.util.Map;
-//抽象出来的接口
-public interface Reduce {
-    java.util.Map<String,Integer> merge(Map<String,Integer>... maps);
-}
